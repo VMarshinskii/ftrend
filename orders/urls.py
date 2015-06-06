26fff64be6a77d42ef7d696895f15258e0894202
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-06-07 01:56:12 +0500

    Update

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', 'orders.views.orders_view'),
     url(r'^create/$', 'orders.views.create_order'),
     url(r'^thanks/$', 'orders.views.thank_order'),
     url(r'^reg_thanks/$', 'orders.views.reg_thank_order'),
