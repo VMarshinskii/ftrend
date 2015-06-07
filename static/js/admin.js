4e7b61218826e6a3816a03b53a3ab1e56a8722f2
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-06-07 11:52:01 +0500

    Update

--- static/js/admin.js
+++ static/js/admin.js
@@ -171,7 +171,7 @@ $(document).ready(function(){
         $(document).on('click', ".field-sizes .item", function () {
 
             $(".field-colors").find(".controls").html('<div class="related-widget-wrapper" id="ajax_size_colors"></div>');
-            $("#ajax_size_colors").load("/admin/size_colors/");
+            $("#ajax_size_colors").load("/admin/size_colors/sada/");
 
             $("#ajax_size_colors").selectize({
                 delimiter: ','
