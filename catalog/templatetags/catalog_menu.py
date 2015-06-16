b8c18230bc499e0a815f033c3671d3eaac6b6438
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-06-16 20:32:03 +0500

    Update

--- catalog/templatetags/catalog_menu.py
+++ catalog/templatetags/catalog_menu.py
@@ -9,7 +9,7 @@ def catalog_menu():
 
     for categ in Category.objects.filter(parent=None):
         new_cat = categ
-        new_cat.childs = Category.objects.filter(parent=categ)
+        new_cat.childs = Category.objects.filter(parent=categ).order_by('title')
         categories.append(new_cat)
 
     return {'categories': categories}
