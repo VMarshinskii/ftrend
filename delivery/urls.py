bf8c385682b89eef635e05bd25eb6b9e0634b42a
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-10 16:41:41 +0500

    Update

--- delivery/urls.py
+++ delivery/urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^list_cdek/$', 'delivery.views.points_sdk_view'),
+    url(r'^list_cdek/(?P<url>[\-\w]+).$', 'delivery.views.points_sdk_view'),
 )
--- delivery/views.py
+++ delivery/views.py
@@ -10,3 +10,11 @@ def points_sdk_view(request):
         # obj = DeliveryCDEK(name=row[0], code=row[1], address=row[2], phone=row[3], mail=row[4], time=row[5])
         # obj.save()
     return render_to_response("points_sdk_view.html")
+
+
+def point_sdk_view(request, url):
+    points = DeliveryCDEK.objects.filter(url=url)
+    return render_to_response("point_sdk_view.html", {
+        'city': points[0].name,
+        'points': points
+    })
