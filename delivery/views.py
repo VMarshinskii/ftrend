551c34ecab3491a36fa0bbc1ceeae0140c1b3da0
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-10 15:18:21 +0500

    Update

--- delivery/views.py
+++ delivery/views.py
@@ -3,11 +3,10 @@ from models import DeliveryCDEK
 import xlrd
 
 def points_sdk_view(request):
-    rb = xlrd.open_workbook('/var/www/ftrend/delivery/sdek.xls', formatting_info=True)
-    sheet = rb.sheet_by_index(0)
-    for rownum in range(sheet.nrows):
-        row = sheet.row_values(rownum)
-        obj = DeliveryCDEK(name=row[0], code=row[1], address=row[2], phone=row[3], mail=row[4], time=row[5])
-        obj.save()
-        print obj.mail
+    # rb = xlrd.open_workbook('/var/www/ftrend/delivery/sdek.xls', formatting_info=True)
+    # sheet = rb.sheet_by_index(0)
+    # for rownum in range(sheet.nrows):
+    #     row = sheet.row_values(rownum)
+        # obj = DeliveryCDEK(name=row[0], code=row[1], address=row[2], phone=row[3], mail=row[4], time=row[5])
+        # obj.save()
     return render_to_response("points_sdk_view.html")
