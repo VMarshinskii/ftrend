80892d2cdc4401c7553d4fdb8b6bd07c6666c646
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-10 15:14:30 +0500

    Update

--- delivery/views.py
+++ delivery/views.py
@@ -7,7 +7,7 @@ def points_sdk_view(request):
     sheet = rb.sheet_by_index(0)
     for rownum in range(sheet.nrows):
         row = sheet.row_values(rownum)
-        for c_el in row:
-            print c_el
-        # DeliveryCDEK(name=row[0], code=row[1], address=row[2], phone=row[3], mail=row[4], time=row[5]).save()
+        obj = DeliveryCDEK(name=row[0], code=row[1], address=row[2], phone=row[3], mail=row[4], time=row[5])
+        obj.save()
+        print obj.mail
     return render_to_response("points_sdk_view.html")
