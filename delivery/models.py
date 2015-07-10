1c49df247ac4c97f36775029a3b89b179b58dfd8
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-10 15:01:47 +0500

    Update

--- delivery/models.py
+++ delivery/models.py
@@ -2,12 +2,11 @@
 from django.db import models
 
 
-class DeliveryCDK(models.Model):
+class DeliveryCDEK(models.Model):
     name = models.CharField(verbose_name="Название", max_length=250)
     code = models.CharField(verbose_name="Код пункта выдачи", max_length=250)
     address = models.CharField(verbose_name="Адрес", max_length=250)
     phone = models.CharField(verbose_name="Телефон", max_length=250)
     mail = models.CharField(verbose_name="Почта", max_length=250)
     time = models.CharField(verbose_name="Время работы", max_length=250)
-    country = models.CharField(verbose_name="Страна", max_length=250, default="russia")
-    public_on_list = models.BooleanField(default=True)
+    url = models.CharField(verbose_name="Url", max_length=250, unique=True, blank=True)
--- delivery/views.py
+++ delivery/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render_to_response
+import xlrd
 
 def points_sdk_view(request):
+    rb = xlrd.open_workbook('sdek.xls', formatting_info=True)
+    sheet = rb.sheet_by_index(0)
+    for rownum in range(sheet.nrows):
+        row = sheet.row_values(rownum)
+        for c_el in row:
+            print c_el
     return render_to_response("points_sdk_view.html")
