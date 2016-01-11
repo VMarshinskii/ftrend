10b78ec713be9d024ef7dda5a048983a744f448f
VMarshinskii <on_VIP5marshinskii@gmail.com>
2016-01-11 21:30:12 +0500

    Update

--- catalog/templatetags/categ_sort.py
+++ catalog/templatetags/categ_sort.py
@@ -2,7 +2,7 @@ from django import template
 from catalog.additions import SORTED_PRODUCT_OPTION
 register = template.Library()
 
-@register.inclusion_tag('templatetags/categ_sort_tag.html')
+@register.inclusion_tag('categ_sort_tag.html')
 def categ_sort(selected):
     return {
         'sorted_options': SORTED_PRODUCT_OPTION,
