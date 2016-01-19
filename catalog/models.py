62035a26023747625d84920843260aedbbb83512
VMarshinskii <on_VIP5marshinskii@gmail.com>
2016-01-19 16:33:24 +0500

    Update

--- catalog/models.py
+++ catalog/models.py
@@ -75,7 +75,7 @@ class Color(models.Model):
 class Brand(models.Model):
     title = models.CharField("Название", max_length=200)
     text = models.TextField("Описание", blank=True, null=True)
-    url = models.CharField("Url", max_length=200, blank=True, unique=True)
+    url = models.CharField("Url", max_length=200, blank=True)
 
     class Meta:
         verbose_name_plural = "Бренды"
@@ -95,7 +95,7 @@ class Age(models.Model):
     type = models.CharField("Для кого", max_length=200, choices=AGE_CHOICES, default='boys')
     title = models.CharField("Название", max_length=200)
     text = models.TextField("Описание", blank=True, null=True)
-    url = models.CharField("Url", max_length=200, blank=True, unique=True)
+    url = models.CharField("Url", max_length=200, blank=True)
 
     class Meta:
         verbose_name_plural = "Возраст"
